d5519365389a16a3eaed92850492b3fc29f815e3
Luis Alejandro Londono Builes <lalondonob@gmail.com>
2021-09-15 17:46:57 -0500

    feat(01): adding project configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,69 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
   },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/quotes': [
+      'error',
+      'single',
+      {
+        allowTemplateLiterals: true,
+        avoidEscape: true,
+      },
+    ],
+    '@typescript-eslint/explicit-member-accessibility': [
+      'off',
+      {
+        accessibility: 'explicit',
+      },
+    ],
+    'import/order': 'off',
+    'max-len': [
+      'error',
+      {
+        code: 150,
+      },
+    ],
+    '@typescript-eslint/member-ordering': 'off',
+    'arrow-parens': ['off', 'always'],
+    '@typescript-eslint/semi': ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    '@typescript-eslint/no-inferrable-types': ['off', 'always'],
+    '@typescript-eslint/explicit-module-boundary-types': ['off', 'always'],
+    '@typescript-eslint/no-explicit-any': ['off', 'always'],
+    '@typescript-eslint/ban-types': ['off', 'always'],
+    'prefer-spread': ['off', 'always'],
+    '@typescript-eslint/no-array-constructor': ['off', 'always'],
+    'no-console': ['error'],
+    'prefer-const': ['off', 'always'],
+    '@typescript-eslint/no-empty-function': ['off', 'always'],
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars-experimental': [
+      'error',
+      { ignoreArgsIfArgsAfterAreUsed: true },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.spec.ts'],
+      rules: {
+        'max-len': [
+          'error',
+          {
+            code: 150,
+            ignoreStrings: true,
+          },
+        ],
+      },
+    },
+  ],
 };
